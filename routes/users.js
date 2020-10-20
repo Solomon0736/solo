0be6e85e4602c01f758a4b52fc705f23fae48a56
solomon <solomon78@gmail.com>
2020-10-20 11:18:55 +0200

    commit

--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,16 @@ var router = express.Router();
 const { query } = require('../models/db');
 
 router.get('/', function (req, res, next) {
+  res.json({
+    status: 200,
+    msg: 'funkar',
+  });
 });
 
 router.get('/:id', async function (req, res, next) {
   try {
     const user = await query(
-      'SELECT * FROM users WHERE id = ?',
+      'SELECT * FROM story WHERE id = ?',
       req.params.id
     );
     
